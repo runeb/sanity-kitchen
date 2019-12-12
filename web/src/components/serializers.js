f582078151ea60ea930668743ed46299b06b4372
Rune Botten <rbotten@gmail.com>
2019-12-12 10:03:26 +0100

    dangling changes

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Figure from "./Figure";
 import Instagram from "./Instagram";
+import YouTube from "./YouTube";
 
 const serializers = {
   types: {
     authorReference: ({ node }) => <span>{node.author.name}</span>,
     mainImage: Figure,
-    instagram: Instagram
+    instagram: Instagram,
+    youtube: YouTube
   }
 };
 
