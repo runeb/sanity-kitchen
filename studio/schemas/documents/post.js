2e5b1fc49aeeaf2a301f6835539abfd8f9b003f2
Rune Botten <rbotten@gmail.com>
2020-03-18 12:32:49 -0700

    remove mux video from post and use master dataset in frontend

--- studio/schemas/documents/post.js
+++ studio/schemas/documents/post.js
@@ -33,11 +33,6 @@ export default {
       title: 'Main image'
     },
     {
-      name: 'video',
-      type: 'mux.video',
-      title: 'Video'
-    },
-    {
       name: 'excerpt',
       type: 'excerptPortableText',
       title: 'Excerpt',
